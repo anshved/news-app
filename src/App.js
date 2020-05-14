32f2bf50f1678cf0bb8cfd2bc4f4ce12e9a3e28b
anshved <ansh.ved@sakec.ac.in>
2020-05-14 19:13:27 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,111 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Image, Container, Dropdown, Menu, Pagination } from 'semantic-ui-react';
+
+import { countries } from './constants/countryList';
+import { categories } from './constants/categories';
+import  image  from './components/news-icon.png';
+import errImg from './nonews.png';
+import { URL, PAGE_SIZE } from './constants/constants';
+import { itemsFetchData, changeCountry, changeCategory, changePage } from './actions';
+import News from './components/News';
 import './App.css';
-import Search from './Search.js';
-import './Search.css';
-import Footer from './Footer.js';
-import './Footer.css';
-import logo from './images/react.svg';
-import news from './images/news.png';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchData(URL+"country="+this.props.country+"&category="
+      +this.props.category+"&pageSize="+PAGE_SIZE
+      +"&page="+this.props.activePage+"&apiKey=940ebc582b1e4205b64ed083e998e723")
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.country!==prevProps.country
+      ||this.props.category!==prevProps.category
+      ||this.props.activePage!==prevProps.activePage) {
+        this.props.fetchData(URL+"country="+this.props.country+"&category="
+          +this.props.category+"&pageSize="+PAGE_SIZE
+          +"&page="+this.props.activePage+"&apiKey=940ebc582b1e4205b64ed083e998e723")
+    }
+  }
+
+
   render() {
-    return (
-      <div className="app">
-        <div className="appHeader">
-          <img className="appLogo" src={logo} alt="React Logo" />
-
-          <div className="title">
-            <h1>&lt;insert fancy name&gt;</h1>
-            <h4>
-              Powered by
-              <a href="https://newsapi.org/"> News API </a>🔥{' '}
-            </h4>
-          </div>
-
-          <img className="scriptLogo" src={news} alt="Javascript Logo" />
+    if(this.props.status!=="error") {
+      return (
+      <div style={{
+        backgroundImage:'url("https://pooreboysingray.files.wordpress.com/2014/11/mobile-tribune-18640001.jpg")',
+        flex:'1'}}
+        >
+        <Menu fluid stackable inverted>
+          <Container>
+            <Menu.Item as='a' href="https://nsharma1396.github.io/react-news-api/" header>
+              <Image
+                src={image}
+                size="mini"
+                style={{ marginRight: '1em' }}
+              />
+              Headlines Today
+            </Menu.Item>
+            <Dropdown
+              openOnFocus
+              inline item placeholder='Category'
+              options={ categories } 
+              onChange = {(ev, {value} ) => this.props.changeCategory(value) } 
+            />
+            <Dropdown
+              openOnFocus
+              inline item placeholder='Country' 
+              value={this.props.country} options={ countries }
+              onChange = {(ev, {value} ) => this.props.changeCountry(value) }
+            />
+          </Container>
+        </Menu>
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+          <Pagination
+            style={{visibility:this.props.status==="loading"?'hidden':'visible'}}
+            activePage={this.props.activePage}
+            ellipsisItem={null}
+            inverted
+            totalPages={this.props.totalResults?Math.ceil(this.props.totalResults/PAGE_SIZE):3}
+            onPageChange={(ev, { activePage }) => this.props.changePage(activePage) } />
         </div>
-        <Search default="in" />
-        <Footer />
-      </div>
-    );
+        <News />
+      </div>  
+      );
+    }
+    else {
+        return (
+          <div style={{marginTop:'10px',
+            display:'flex',flexDirection:'column',
+            justifyContent:'center',
+            alignItems:'center'}}
+          >
+            <Image src={errImg} size="big" centered />
+            <h1 style={{textAlign:'center'}}><br/>No News Available as some error has occured !! </h1>
+          </div>
+      )
+    }
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    status: state.status,
+    totalResults: state.data.totalResults,
+    country: state.country,
+    category: state.category,
+    activePage: state.activePage
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeCountry: (countryName) => dispatch(changeCountry(countryName)),
+    changeCategory: (categoryName) => dispatch(changeCategory(categoryName)),
+    changePage: (activePage) => dispatch(changePage(activePage)),
+    fetchData: (url) => dispatch(itemsFetchData(url))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
