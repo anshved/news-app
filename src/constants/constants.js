90b76ab2af69243fee604c2992b9e135fbbef0ea
anshved <ansh.ved@sakec.ac.in>
2020-05-23 16:05:20 +0530

    cors error solved!

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,4 +5,4 @@ export const COUNTRY_CHANGED = 'COUNTRY_CHANGED';
 export const CATEGORY_CHANGED = 'CATEGORY_CHANGED';
 export const PAGE_CHANGED = 'PAGE_CHANGED';
 export const PAGE_SIZE = 10;
-export const URL = 'https://newsapi.org/v2/top-headlines?';
+export const URL = 'https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?';
